6bd229c3e90e2973df0d813ae04b94cc63587ac9
Korsunow Konstantin <korsunowk@yandex.ua>
2016-09-29 15:43:44 +0300

    Added refresh captcha

--- static/js/one.js
+++ static/js/one.js
@@ -1,13 +1,17 @@
 /**
  * Created by 1 on 26.03.2016.
  */
+$(document).ready(function() {
+    $('img.captcha').click(function(){
+        var $form = $(this).parents('form');
+        var url = location.protocol + "//" + window.location.hostname + ":"
+                  + location.port + "/captcha/refresh/";
 
-$('.js-captcha-refresh').click(function(){
-        $form = $(this).parents('form');
-
-        $.getJSON($(this).data('url'), {}, function(json) {
-            // This should update your captcha image src and captcha hidden input
+        $.getJSON(url, {}, function(json) {
+            $form.find('input[name="captcha_0"]').val(json.key);
+            $form.find('img.captcha').attr('src', json.image_url);
         });
 
         return false;
     });
+});
